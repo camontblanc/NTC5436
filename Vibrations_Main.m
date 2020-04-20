b8e850655a51718b64c904f6ce70d3625ed82758
camontblanc <45245175+camontblanc@users.noreply.github.com>
2020-04-20 09:14:30 -0500

    Update Vibrations_Main.m

--- Vibrations_Main.m
+++ Vibrations_Main.m
@@ -1,11 +1,11 @@
 clear all; close all; clc
 
-% Cargamos las datos de aceleraci髇. Hay 4 filas distribuidas de la
+% Cargamos las datos de aceleraci贸n. Hay 4 filas distribuidas de la
 % siguiente manera:
 %       1. Vector de tiempo en s.
-%       2. Aceleraci髇 longitudinal (eje x) en g's.
-%       3. Aceleraci髇 vertical (eje y) en g's.
-%       4. Aceleraci髇 horizantal (eje z) en g's.
+%       2. Aceleraci贸n longitudinal (eje x) en g's.
+%       3. Aceleraci贸n vertical (eje y) en g's.
+%       4. Aceleraci贸n horizantal (eje z) en g's.
 load ('ABHigh_Seatpost_Accel.mat')
 t  = ADC(1,:)';
 ax = ADC(2,:)';
@@ -18,13 +18,13 @@ ax = (ax-mean(ax))*9.81;
 ay = (ay-mean(ay))*9.81;
 az = (az-mean(az))*9.81;
 
-% Obtenemos la frecuencia de muestreo del sensor. Miramos cu醤to tiempo se
+% Obtenemos la frecuencia de muestreo del sensor. Miramos cu谩nto tiempo se
 % demora en tomar dos datos contiguos.
 fs=1/mean(diff(t));
 
 %% Procesamiento en el dominio del tiempo
 
-% Frecuencias de la funci髇 de transferencia Wk de las ponderaciones de las
+% Frecuencias de la funci贸n de transferencia Wk de las ponderaciones de las
 % frecuencias principales - Tabla A.1 NTC 5436-1
 freqs = [0.4 100 12.5 12.5 2.37 3.35];
 ws = 2*pi*freqs;
@@ -34,52 +34,37 @@ w1 = ws(1); w2 = ws(2); w3 = ws(3); w4 = ws(4); w5 = ws(5); w6 = ws(6);
 Qs = [0.63 0.91 0.91];
 Q4 = Qs(1); Q5 = Qs(2); Q6 = Qs(3);
 
-% Funciones de transferencia: transici髇 a-v y etapa ascedente
+% Funciones de transferencia: transici贸n a-v y etapa ascedente
 Ht = tf([0,1/w3,1],[(1/w4)^2,1/(Q4*w4),1]);
 Hs = tf([1,w5/Q5,w5^2],[1,w6/Q6,w6^2]);
-Hts=Ht*Hs;                              % Teorema de convoluci髇
+Hts=Ht*Hs;                              % Teorema de convoluci贸n
 [bts,ats] = tfdata(Hts,'v');
-% M閠odo de transformaci髇 bilineal para la conversi髇 de filtros 
-% an醠ogos a digitales
+% M茅todo de transformaci贸n bilineal para la conversi贸n de filtros 
+% an谩logos a digitales
 [Bts,Ats] = bilinear(bts,ats,fs);
-ats = filter(Bts,Ats,ay);               % Filtramos ay con la funci髇 de transferencia
+ats = filter(Bts,Ats,ay);               % Filtramos ay con la funci贸n de transferencia
                                         % cuyo denominador es Ats y
                                         % numerador Bts
 
 % Funciones de transferencia: paso alto y paso bajo
 Hl = tf([0,0,1],[(1/w2)^2,sqrt(2)/w2,1]);
 Hh = tf([1,0,0],[1,sqrt(2)*w1,w1^2]);
-Hlh=Hl*Hh;                              % Limitaci髇 en banda
+Hlh=Hl*Hh;                              % Limitaci贸n en banda
 [blh,alh] = tfdata(Hlh,'v');
 [Blh,Alh] = bilinear(blh,alh,fs);
-aw = filter(Blh,Alh,ats);               % Filtramos ats con la funci髇 de transferencia
+aw = filter(Blh,Alh,ats);               % Filtramos ats con la funci贸n de transferencia
                                         % cuyo denominador es Alh y
                                         % numerador Blh
 
-% C醠culo de la aceleraci髆 rms filtrada (en el dominio del tiempo)
+% C谩lculo de la aceleraci贸m rms filtrada (en el dominio del tiempo)
 a_rms_time = sqrt(trapz(t,aw.^2)/t(end));
-fprintf('La aceleraci髇 rms es = %1.2f m/s^2\n', aw)
+fprintf('La aceleraci贸n rms es = %1.2f m/s^2\n', a_rms_time)
 
-% Factor de cresta: M骴ulo de la relaci髇 entre el valor pico instant醤eo
-% m醲imo de la se馻l de aceleraci髇 de frecuencia ponderada y su valor rms.
-% El factor de cresta NO indics la severidad de la vibraci髇.
+% Factor de cresta: M贸dulo de la relaci贸n entre el valor pico instant谩neo
+% m谩ximo de la se帽al de aceleraci贸n de frecuencia ponderada y su valor rms.
+% El factor de cresta NO indics la severidad de la vibraci贸n.
 CF = max(abs(aw))/a_rms_time;
 
-% Dosis de vibraci髇 a la cuarta potencia
+% Dosis de vibraci贸n a la cuarta potencia
 VDV = (trapz(t,aw.^4))^(1/4);
-fprintf('La dosis de vibraci髇 a la cuarta potencia es = %1.2f m/s^1.75\n', VDV)
-
-%% Procesamiento en el dominio de la frecuencia
-
-% Estimaci髇 de la densidad espectral de potencia de Welch. En el caso 
-% de la densidad espectral, el resultado debe ser escalado por la frecuencia 
-% de muestreo, que no se realiza por psd.
-[Y,f] = pwelch(ay,window(@rectwin,round(fs)),[],[],fs);
-
-% Ponderaci髇 de la densidad espectral por medio de las magnitudes de las
-% funciones de transferencia en dB. Es decir, densidad espectral de
-% potencia de la se馻l ponderada.
-weighPSD = ISOseatVER(Y,f);
-
-% C醠culo de la aceleraci髇 rms en el dominio de la frecuencia.
-a_rms_freq = sqrt(trapz(f,weighPSD));
+fprintf('La dosis de vibraci贸n a la cuarta potencia es = %1.2f m/s^1.75\n', VDV)
